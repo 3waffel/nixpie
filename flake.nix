484f9a4a74c1d92900d082eafd69cbc63459dc66
ワフ <45911671+3waffel@users.noreply.github.com>
2022-09-05 08:11:13 +0000

    add nixos-generators

--- flake.nix
+++ flake.nix
@@ -7,6 +7,13 @@
       url = "github:edolstra/flake-compat";
       flake = false;
     };
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixos-hardware = {
+      url = "github:NixOS/nixos-hardware";
+    };
     nixconfig = {
       url = "github:3waffel/nixconfig";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -18,6 +25,9 @@
     nixpkgs,
     flake-utils,
     devshell,
+    nixos-generators,
+    nixos-hardware,
+    nixconfig,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (
@@ -28,7 +38,27 @@
             devshell.overlay
           ];
         };
-      in {
+        pkgs-rpi = import nixpkgs {
+          system = "aarch64-linux";
+          overlays = [
+            (final: super: {
+              makeModulesClosure = x:
+                super.makeModulesClosure (x // {allowMissing = true;});
+            })
+          ];
+        };
+      in rec {
+        packages.rpi4 = nixos-generators.nixosGenerate {
+          pkgs = pkgs-rpi;
+          format = "sd-aarch64";
+          modules = [nixos-hardware.nixosModules.raspberry-pi-4];
+        };
+        packages.rpi4-installer = nixos-generators.nixosGenerate {
+          pkgs = pkgs-rpi;
+          format = "sd-aarch64-installer";
+          modules = [nixos-hardware.nixosModules.raspberry-pi-4];
+        };
+        packages.default = packages.rpi4;
         devShells.default = pkgs.devshell.mkShell {
           imports = [
             (pkgs.devshell.importTOML ./devshell.toml)
