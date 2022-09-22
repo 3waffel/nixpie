ff90788311f1bf629e0baba79362771098aeab32
ワフ <45911671+3waffel@users.noreply.github.com>
2022-09-22 09:23:36 +0000

    fix up pkgs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     devshell.url = "github:numtide/devshell";
     flake-compat = {
@@ -11,11 +12,9 @@
       url = "github:nix-community/nixos-generators";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixos-hardware = {
-      url = "github:NixOS/nixos-hardware";
-    };
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
     nixconfig = {
-      url = "github:3waffel/nixconfig";
+      url = "github:3waffel/nixconfig/raspi";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -23,47 +22,37 @@
   outputs = {
     self,
     nixpkgs,
+    nixpkgs-unstable,
     flake-utils,
     devshell,
     nixos-generators,
     nixos-hardware,
     nixconfig,
     ...
-  }:
-    flake-utils.lib.eachDefaultSystem (
+  } @ flakes:
+    flake-utils.lib.eachSystem ["aarch64-linux"] (
       system: let
-        pkgs = import nixpkgs {
+        stable = import nixpkgs {
           inherit system;
-          overlays = [
-            devshell.overlay
-          ];
         };
-        pkgs-rpi = import nixpkgs {
-          system = "aarch64-linux";
-          overlays = [
-            (final: super: {
-              makeModulesClosure = x:
-                super.makeModulesClosure (x // {allowMissing = true;});
-            })
-          ];
+        unstable = import nixpkgs-unstable {
+          inherit system;
         };
       in rec {
         packages.rpi4 = nixos-generators.nixosGenerate {
-          pkgs = pkgs-rpi;
+          pkgs = stable;
+          modules = [
+            nixos-hardware.nixosModules.raspberry-pi-4
+            ./images/raspi.nix
+          ];
           format = "sd-aarch64";
-          modules = [nixos-hardware.nixosModules.raspberry-pi-4];
         };
         packages.rpi4-installer = nixos-generators.nixosGenerate {
-          pkgs = pkgs-rpi;
+          pkgs = stable;
           format = "sd-aarch64-installer";
           modules = [nixos-hardware.nixosModules.raspberry-pi-4];
         };
         packages.default = packages.rpi4;
-        devShells.default = pkgs.devshell.mkShell {
-          imports = [
-            (pkgs.devshell.importTOML ./devshell.toml)
-          ];
-        };
       }
     );
 }
---
+++ images/raspi.nix
@@ -0,0 +1,27 @@
+{
+  lib,
+  nixos-hardware,
+  ...
+}: {
+  nixpkgs.localSystem = {
+    config = "aarch64-unknown-linux-gnu";
+    system = "aarch64-linux";
+  };
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/NIXOS_SD";
+      fsType = "ext4";
+      options = ["noatime"];
+    };
+  };
+  swapDevices = [];
+
+  powerManagement.cpuFreqGovernor = lib.mkDefault "ondemand";
+
+  networking.wireless.enable = false;
+
+  hardware.pulseaudio.enable = false;
+
+  documentation.enable = false;
+}
